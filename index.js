e4d97142e669b300ae80d0f923ce7683ccf79abc
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-11 11:01:44 -0500

    Tweet Boilerplate challenge

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js.
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
 
-import tweetsData from './data.js';
 
-console.log(tweetsData);
+import { tweetsData } from './data.js';
+const tweetBtn = document.querySelector('#tweet-btn');
+const tweetInput = document.querySelector('#text-input');
 
+tweetBtn.addEventListener('click', () => {
+  const tweetText = tweetInput.value;
+  tweetInput.value = "";
+  console.log(tweetText);
+})
+
+const getFeedHtml = () => {
+  tweetsData.forEach(tweet => {
+    const feedHtml = `
+    <div class="tweet">
+    <div class="tweet-inner">
+        <img src=${tweet.profilePic}class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                  ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                  ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+    console.log(feedHtml);
+  })
+}
+
+getFeedHtml();
